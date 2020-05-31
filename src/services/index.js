8e6f6844a490d4a88a30ecd52877beb33c893be1
Diogenes <diogensgreen@gmail.com>
2020-05-31 17:00:59 -0300

    refact:
    remove link api

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-export const api = axios.create({
+/* export const api = axios.create({
   baseURL: "https://servicodados.ibge.gov.br/api/v2"
-});
-
+}); */
 
 export const corona1 = axios.create({
   baseURL: "https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com"
